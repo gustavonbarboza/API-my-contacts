231b5844e65068f3d6874262053d09fe31285f3c
Gustavo Barboza <gustabarboza37@gmail.com>
2025-06-12 23:46:50 -0300

    Criada a rota de edição dos contatos

--- src/app/controllers/CotactController.js
+++ src/app/controllers/CotactController.js
@@ -27,13 +27,12 @@ class ContacController {
     } = request.body;
 
     if (!name) {
-      return response.status(400).json({ email: 'Name is required' });
+      return response.status(400).json({ error: 'Name is required' });
     }
 
     const contactExists = await ContactsRepository.findByEmail(email);
-
     if (contactExists) {
-      return response.status(400).json({ email: 'This email is already been taken' });
+      return response.status(400).json({ error: 'This email is already in use' });
     }
 
     const contact = await ContactsRepository.create({
@@ -44,7 +43,31 @@ class ContacController {
   }
 
   // Editar um registro
-  update() {
+  async update(request, response) {
+    const { id } = request.params;
+    const {
+      name, email, phone, category_id,
+    } = request.body;
+
+    const contactExists = await ContactsRepository.findById(id);
+    if (!contactExists) {
+      return response.status(400).json({ error: 'User not found' });
+    }
+
+    if (!name) {
+      return response.status(400).json({ error: 'Name is required' });
+    }
+
+    const contactByEmail = await ContactsRepository.findByEmail(email);
+    if (contactByEmail && contactByEmail.id !== id) {
+      return response.status(400).json({ error: 'This email is already in use' });
+    }
+
+    const contact = await ContactsRepository.update(id, {
+      name, email, phone, category_id,
+    });
+
+    response.json(contact);
   }
 
   // Deletar um registro
