e8fb1d0c96cdc31722dfba4360eda9756f3d56a0
Gustavo Barboza <gustabarboza37@gmail.com>
2025-06-30 12:44:58 -0300

    Adicionado o Header principal com a logo e o Header do ContactsList

---
+++ fe/src/components/Header/index.js
@@ -0,0 +1,15 @@
+import { Container, InputSeachContainer } from './styles';
+
+import logo from '../../assets/images/logo.svg';
+
+export default function Header() {
+  return (
+    <Container>
+      <img src={logo} alt="MyContacts" width="201" />
+
+      <InputSeachContainer>
+        <input type="text" placeholder="Pesquisar pelo nome..." />
+      </InputSeachContainer>
+    </Container>
+  );
+}
