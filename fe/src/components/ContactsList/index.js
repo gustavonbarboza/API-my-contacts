edd7422f51bf18ebe23d58321eba50e4aea4a983
Gustavo Barboza <gustabarboza37@gmail.com>
2025-07-01 01:07:21 -0300

    Adicionado a listagem de contatos

--- fe/src/components/ContactsList/index.js
+++ fe/src/components/ContactsList/index.js
@@ -1,12 +1,52 @@
-import { Container, Header } from './styles';
+import {
+  Container, Header, ListContainer, Card,
+} from './styles';
+
+import arrow from '../../assets/images/icons/arrow.svg';
+import edit from '../../assets/images/icons/edit.svg';
+import trash from '../../assets/images/icons/trash.svg';
 
 export default function ContactsList() {
   return (
     <Container>
+
       <Header>
         <strong>3 contatos</strong>
         <a href="/">Novo contato </a>
       </Header>
+
+      <ListContainer>
+        <header>
+          <button type="button">
+            <span>Nome</span>
+            <img src={arrow} alt="arrow" />
+          </button>
+        </header>
+
+        <Card>
+          <div className="info">
+            <div className="contact-name">
+              <strong>Mateus Silva</strong>
+              <small>Instagram</small>
+            </div>
+
+            <span>mateus@email.com.br</span>
+            <span>(61) 9991242412</span>
+          </div>
+
+          <div className="actions">
+            <a href="/">
+              <img src={edit} alt="Edit" />
+            </a>
+
+            <button type="button">
+              <img src={trash} alt="Delele" />
+            </button>
+          </div>
+        </Card>
+
+      </ListContainer>
+
     </Container>
   );
 }
