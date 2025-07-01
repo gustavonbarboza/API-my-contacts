6a798ef7b08711a6f55f1cbcc2afb1a3b2550302
Gustavo Barboza <gustabarboza37@gmail.com>
2025-07-01 13:15:36 -0300

    Criado o componente ContactForm

--- fe/src/pages/EditContact/index.js
+++ fe/src/pages/EditContact/index.js
@@ -1,7 +1,14 @@
 import PageHeader from '../../components/PageHeader';
+import ContactForm from '../../components/ContactForm';
 
 export default function EditContact() {
   return (
-    <PageHeader title="Editar Mateus Silva" />
+    <>
+      <PageHeader title="Editar Mateus Silva" />
+
+      <ContactForm
+        buttonLabel="Salvar alterações"
+      />
+    </>
   );
 }
--- fe/src/pages/Home/index.js
+++ fe/src/pages/Home/index.js
@@ -1,3 +1,5 @@
+import { Link } from 'react-router-dom';
+
 import {
   Container, InputSeachContainer, Header, ListContainer, Card,
 } from './styles';
@@ -16,7 +18,7 @@ export default function Home() {
 
       <Header>
         <strong>3 contatos</strong>
-        <a href="/">Novo contato </a>
+        <Link to="/new">Novo contato</Link>
       </Header>
 
       <ListContainer>
@@ -39,9 +41,9 @@ export default function Home() {
           </div>
 
           <div className="actions">
-            <a href="/">
+            <Link to="/edit/123">
               <img src={edit} alt="Edit" />
-            </a>
+            </Link>
 
             <button type="button">
               <img src={trash} alt="Delele" />
