24b1683539659617357d27f6e225fb650ef96b13
Gustavo Barboza <gustabarboza37@gmail.com>
2025-07-03 23:16:47 -0300

    Adicionado regra de ativação do botão do submit do form quando estiver válido

--- fe/src/components/ContactForm/index.js
+++ fe/src/components/ContactForm/index.js
@@ -17,7 +17,12 @@ export default function ContactForm({ buttonLabel }) {
   const [email, setEmail] = useState('');
   const [phone, setPhone] = useState('');
   const [category, setCategory] = useState('');
-  const { setError, removeError, getErrorMessageByFieldName } = useErrors();
+
+  const {
+    setError, removeError, getErrorMessageByFieldName, errors,
+  } = useErrors();
+
+  const isFormValid = (name && errors.length === 0);
 
   function handleNameChange(event) {
     setName(event.target.value);
@@ -46,9 +51,9 @@ export default function ContactForm({ buttonLabel }) {
   function handleSubmit(event) {
     event.preventDefault();
 
-    console.log({
-      name, email, phone: phone.replace(/\D/g, ''), category,
-    });
+    // console.log({
+    //   name, email, phone: phone.replace(/\D/g, ''), category,
+    // });
   }
 
   return (
@@ -57,7 +62,7 @@ export default function ContactForm({ buttonLabel }) {
       <FormGroup error={getErrorMessageByFieldName('name')}>
         <Input
           error={getErrorMessageByFieldName('name')}
-          placeholder="Nome"
+          placeholder="Nome *"
           value={name}
           onChange={handleNameChange}
         />
@@ -94,7 +99,7 @@ export default function ContactForm({ buttonLabel }) {
       </FormGroup>
 
       <ButtonContainer>
-        <Button type="submit">
+        <Button type="submit" disabled={!isFormValid}>
           {buttonLabel}
         </Button>
       </ButtonContainer>
