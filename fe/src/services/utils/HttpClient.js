b1e212b7ff3284d97af38586544b7f36f82120c0
Gustavo Barboza <gustabarboza37@gmail.com>
2025-07-22 11:07:32 -0300

    Criado o parse do body baseado no Content-Type

--- fe/src/services/utils/HttpClient.js
+++ fe/src/services/utils/HttpClient.js
@@ -10,12 +10,21 @@ class HttpClient {
 
     const response = await fetch(`${this.baseUrl}${path}`);
 
-    // response.status >= 200 && response.status <= 299
-    if (response.ok) {
-      return response.json();
+    let body = null;
+    const contentType = response.headers.get('Content-Type');
+
+    if (contentType.includes('application/json')) {
+      body = await response.json();
     }
 
-    throw new Error(`${response.status} - ${response.statusText}`);
+    // response.status >= 200 && response.status <= 299
+    if (response.ok) {
+      return body;
+    }
+
+    throw new Error(
+      body?.error || `${response.status} - ${response.statusText}`,
+    );
   }
 }
 
