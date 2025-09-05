bd87f8d761ca56761fc706c32eaa45bb0999f7f9
Gustavo Barboza <gustabarboza37@gmail.com>
2025-09-05 01:03:50 -0300

    Carregando os dados do contato na página de edição

--- fe/src/components/ContactForm/index.js
+++ fe/src/components/ContactForm/index.js
@@ -1,5 +1,7 @@
 import PropTypes from 'prop-types';
-import { useState, useEffect } from 'react';
+import {
+  useState, useEffect, forwardRef, useImperativeHandle,
+} from 'react';
 
 import isEmailValid from '../../utils/isEmailValid';
 import formatPhone from '../../utils/formatPhone';
@@ -14,7 +16,7 @@ import Input from '../Input';
 import Select from '../Select';
 import Button from '../Button';
 
-export default function ContactForm({ buttonLabel, onSubmit }) {
+const ContactForm = forwardRef(({ buttonLabel, onSubmit }, ref) => {
   const [name, setName] = useState('');
   const [email, setEmail] = useState('');
   const [phone, setPhone] = useState('');
@@ -28,6 +30,27 @@ export default function ContactForm({ buttonLabel, onSubmit }) {
   } = useErrors();
 
   const isFormValid = (name && errors.length === 0);
+
+  useImperativeHandle(ref, () => ({
+    setFieldsValue: (contact) => {
+      setName(contact.name);
+      setEmail(contact.email);
+      setPhone(contact.phone);
+      setCategoryId(contact.category_id);
+    },
+  }), []);
+
+  // useEffect(() => {
+  //   const refObject = ref;
+  //   refObject.current = {
+  //     setFieldsValue: (contact) => {
+  //       setName(contact.name);
+  //       setEmail(contact.email);
+  //       setPhone(contact.phone);
+  //       setCategoryId(contact.category_id);
+  //     },
+  //   };
+  // });
 
   useEffect(() => {
     async function loadCategories() {
@@ -143,9 +166,11 @@ export default function ContactForm({ buttonLabel, onSubmit }) {
 
     </Form>
   );
-}
+});
 
 ContactForm.propTypes = {
   buttonLabel: PropTypes.string.isRequired,
   onSubmit: PropTypes.func.isRequired,
 };
+
+export default ContactForm;
--- fe/src/pages/EditContact/index.js
+++ fe/src/pages/EditContact/index.js
@@ -1,13 +1,53 @@
+import { useParams, useHistory } from 'react-router-dom/cjs/react-router-dom.min';
+import { useEffect, useRef, useState } from 'react';
+
 import PageHeader from '../../components/PageHeader';
 import ContactForm from '../../components/ContactForm';
+import Loader from '../../components/Loader';
+
+import ContactsService from '../../services/ContactsService';
+import toast from '../../utils/toast';
 
 export default function EditContact() {
+  const [isLoading, setIsLoading] = useState(true);
+  const contactFormRef = useRef(null);
+
+  const { id } = useParams();
+  const history = useHistory();
+
+  useEffect(() => {
+    async function loadContact() {
+      try {
+        const contact = await ContactsService.getContactById(id);
+
+        contactFormRef.current.setFieldsValue(contact);
+        setIsLoading(false);
+      } catch {
+        history.push('/');
+        toast({
+          type: 'danger',
+          text: 'Contato não encontrado',
+        });
+      }
+    }
+
+    loadContact();
+  }, [id, history]);
+
+  function handleSubmit() {
+    console.log('handleSubmit');
+  }
+
   return (
     <>
+      <Loader isLoading={isLoading} />
+
       <PageHeader title="Editar Mateus Silva" />
 
       <ContactForm
+        ref={contactFormRef}
         buttonLabel="Salvar alterações"
+        onSubmit={handleSubmit}
       />
     </>
   );
