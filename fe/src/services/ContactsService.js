85b4ee0f36553099fdc437b3ce1378f03a72e993
Gustavo Barboza <gustabarboza37@gmail.com>
2025-09-05 10:41:06 -0300

    Implementado a edição de contatos

--- fe/src/services/ContactsService.js
+++ fe/src/services/ContactsService.js
@@ -5,16 +5,23 @@ class ContactsServices {
     this.httpClient = new HttpClient('http://localhost:3001');
   }
 
+  // Lista todos os contatos
   listContacts(orderBy = 'asc') {
     return this.httpClient.get(`/contacts/?orderBy=${orderBy}`);
   }
 
+  // Lista o contato com base no id
   getContactById(id) {
     return this.httpClient.get(`/contacts/${id}`);
   }
 
+  // Cria um novo contato
   createContacts(contact) {
     return this.httpClient.post('/contacts', { body: contact });
+  }
+
+  updateContact(id, contact) {
+    return this.httpClient.put(`/contacts/${id}`, { body: contact });
   }
 }
 
